671a6a7fcfd9b7fa23e49b49ff6bd9f4d541329e
ergeakova <ergeak@gmail.com>
2022-07-22 19:45:32 +0300

    Initial Commit

---
+++ WhatsAppCloneSwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  WhatsAppCloneSwiftUI
+//
+//  Created by Erge Gevher Akova on 22.07.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ WhatsAppCloneSwiftUI/WhatsAppCloneSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  WhatsAppCloneSwiftUIApp.swift
+//  WhatsAppCloneSwiftUI
+//
+//  Created by Erge Gevher Akova on 22.07.2022.
+//
+
+import SwiftUI
+
+@main
+struct WhatsAppCloneSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
